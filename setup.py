b67762f753767925e7f0b30f843999d23f55e03f
alexandrebarbaruiva <alexandrebarbaruiva@gmail.com>
2020-06-27 12:46:05 -0300

    Bump release to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rasa-plus",
-    version="0.0.6",
+    version="0.1.0",
     author="Alexandre Augusto de Sá dos Santos",
     author_email="alexandrebarbaruiva@gmail.com",
     description="An organizer for your rasa bot dialogs",
--- tasks.py
+++ tasks.py
@@ -2,6 +2,6 @@ from invoke import task
 
 
 @task
-def uni(c):
+def upload(c):
     c.run("python3 setup.py sdist bdist_wheel")
     c.run("python3 -m twine upload --skip-existing --repository testpypi dist/*")
