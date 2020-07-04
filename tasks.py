5afcba6a314659ee7e94ebb190852562a6df3e75
alexandrebarbaruiva <alexandrebarbaruiva@gmail.com>
2020-07-04 11:16:11 -0300

    Add and improve invoke tasks

--- tasks.py
+++ tasks.py
@@ -1,7 +1,24 @@
 from invoke import task
+import shutil
+
+
+@task
+def clean(c):
+    shutil.rmtree("dist/", ignore_errors=True)
+    shutil.rmtree("build/", ignore_errors=True)
+    shutil.rmtree("rasa_plus.egg-info/", ignore_errors=True)
+    shutil.rmtree("rasa_plus/__pycache__/", ignore_errors=True)
 
 
 @task
 def upload(c):
+    clean(c)
     c.run("python3 setup.py sdist bdist_wheel")
     c.run("python3 -m twine upload --skip-existing --repository testpypi dist/*")
+
+
+@task
+def release(c):
+    clean(c)
+    c.run("python3 setup.py sdist bdist_wheel")
+    c.run("python3 -m twine upload --skip-existing dist/*")
