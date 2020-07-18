00b5d07d72791b5f4c6c0be3b2ca9154c182cca3
alexandrebarbaruiva <alexandrebarbaruiva@gmail.com>
2020-07-18 20:36:14 -0300

    Finish adding commands for project

--- rasa_plus/__main__.py
+++ rasa_plus/__main__.py
@@ -13,19 +13,7 @@ def _get_files(path, type=None):
     return files
 
 
-def _unify_domain(path):
-    result = ""
-    for file in _get_files(path):
-        with open(file, "r") as content:
-            c = content.read().rstrip()
-            if not c.endswith("\n"):
-                c += "\n"
-            c += "\n"
-            result += c
-    return result
-
-
-def _unify_nlu(path):
+def _unify_files(path):
     result = ""
     for file in _get_files(path):
         with open(file, "r") as content:
@@ -51,7 +39,7 @@ def rasa_plus():  # pragma: no cover
 
 @rasa_plus.command()
 def unify_domain(path="./domain", to=".", filename="domain.yml"):  # pragma: no cover
-    content = _unify_domain(path)
+    content = _unify_files(path)
     _generate_file(to, filename, content)
     click.echo("File domain.yml created successfully.")
     return "OK"
@@ -59,9 +47,9 @@ def unify_domain(path="./domain", to=".", filename="domain.yml"):  # pragma: no 
 
 @rasa_plus.command()
 def unify_nlu(path="./data/nlu", to="./data", filename="nlu.md"):  # pragma: no cover
-    content = _unify_nlu(path)
+    content = _unify_files(path)
     _generate_file(to, filename, content)
-    click.echo("File domain.yml created successfully.")
+    click.echo("File nlu.md created successfully.")
     return "OK"
 
 
@@ -69,9 +57,18 @@ def unify_nlu(path="./data/nlu", to="./data", filename="nlu.md"):  # pragma: no 
 def unify_stories(
     path="./data/stories", to="./data", filename="stories.md"
 ):  # pragma: no cover
-    click.echo("Function not yet implemented.")
-    return
+    content = _unify_files(path)
+    _generate_file(to, filename, content)
+    click.echo("File stories.md created successfully.")
+    return "OK"
 
+
+@rasa_plus.command()
+def unify_project():  # pragma: no cover
+    unify_domain()
+    unify_nlu()
+    unify_stories()
+    return "OK"
 
 if __name__ == "__main__":  # pragma: no cover
     rasa_plus()
